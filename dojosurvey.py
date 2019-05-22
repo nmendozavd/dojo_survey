2544bd7766c0363d6e5bfac28d21714270e0ece3
Noel Mendoza <nmendozavd@gmail.com>
2019-05-21 19:24:43 -0700

    updated with read me

--- dojosurvey.py
+++ dojosurvey.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask, render_template, request, redirect, flash, session
 
 app = Flask(__name__)
@@ -17,19 +18,40 @@ def yourname():
 
     validation_failed = False
 
-    if len(session['name']) < 1:
-        flash("Name cannot be blank!")
-        validation_failed = True
-    if len(session['comment']) < 1:
-        flash("Comment cannot be blank")
-        validation_failed = True
-    if len(session['comment']) > 120:
-        flash("Comment cannot be longer than 120 characters!")
-        validation_failed = True
+if len(session['name']) < 1: 
+    flash("Name cannot be blank!") validation_failed=True 
+if len(session['comment']) < 1:
+    flash("Comment cannot be blank") validation_failed=True 
+if len(session['comment'])> 120:
+    flash("Comment cannot be longer than 120 characters!")
+    validation_failed = True
 
-    if validation_failed == True:
-        return redirect('/')
-    else:
-        return render_template('results.html')
+if validation_failed == True:
+    return redirect('/')
+else:
+    return render_template('results.html')
 
+app.run(debug=True)
+
+    """
+
+
+from flask import Flask, render_template, redirect, request 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+@app.route('/results', methods=["POST"])
+def results():
+    return render_template('results.html',
+
+    name = request.form['name'],
+    location = request.form['location'],
+    language = request.form['language'],
+    comment = request.form['comment']
+
+    )
 app.run(debug=True)
